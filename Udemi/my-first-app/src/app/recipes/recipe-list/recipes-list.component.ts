1194b5bb78ae81c7a59b1078b0a16cfe1f01d622
Abhilash Kelageri <abhiabhilash74658.com>
2024-07-30 17:44:59 +0530

    Added components and heasder

---
+++ Udemi/my-first-app/src/app/recipes/recipe-list/recipes-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.type';
+
+@Component({
+  selector: 'app-recipes-list',
+  templateUrl: './recipes-list.component.html',
+  styleUrl: './recipes-list.component.css'
+})
+export class RecipesListComponent implements OnInit {
+
+ recipes: Recipe[] = [
+  new Recipe('A Test Recipe', 'This is for test'),
+  new Recipe('A Test Recipe', 'This is for test')
+
+ ];
+ imagePath = 'assets/Recipe_logo.jpeg';
+  
+  ngOnInit() {
+    
+  }
+}
