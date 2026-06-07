3697fc73fe15c45b648343536a0305fdf8803bff
abhilashk12 <abhiabhilash74658@gmail.com>
2026-06-07 19:04:33 +0530

    Add count management methods to AppComponent

--- Udemi/my-first-app/src/app/app.component.ts
+++ Udemi/my-first-app/src/app/app.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
- 
+
+  title: string = 'Angular Demo App';
+
+  count: number = 0;
+
+  constructor() {
+    console.log('Constructor Called');
+  }
+
+  ngOnInit(): void {
+    console.log('Component Initialized');
+  }
+
+  increment(): void {
+    this.count++;
+  }
+
+  decrement(): void {
+    this.count--;
+  }
+
+  reset(): void {
+    this.count = 0;
+  }
+  
 }
